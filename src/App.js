e4f20d8472032ff21e536d9e0d4ee1583670883c
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-08 13:10:37 +0200

    create react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hi There!
+        <div id="test"> Testing </div>
+      </div>
+    );
+  }
+}
+
+export default App;
